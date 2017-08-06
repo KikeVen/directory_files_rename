5f17e51dac3cf4b0acaf0146ffb87fb448ec6de5
KikeVen <bruzual.enrique@outlook.com>
2017-08-06 14:55:44 -0400

    doc string update

--- file_rename.py
+++ file_rename.py
@@ -20,11 +20,12 @@ day_formated = day_formated.strftime("%m%d%Y")
 
 def file_rename():
     """
-    Lists all files in a specified directory (source_directory). Separates the file extension
-    from the file name (file_extension). If the file extensions are not includes in the exclusion 
-    list, then renames the files with the original file extension into another specified 
-    directory (destination_directory) with a new name based on enumerator (my_enumerator), 
-    formated date (day_formated), time (time_formated) and original file extension (file_extension)
+    Lists all files in a specified directory [source_directory] . Separates the file
+    extension from the file name [file_extension]. If the file extensions are not 
+    includes in the exclusion list, then renames the files with the original file 
+    extension into another specified directory [destination_directory]. The new 
+    file name is based on enumerator [my_enumerator], formated date [day_formated], 
+    time [time_formated] and original file extension [file_extension]
     """
 
     my_enumerator = 1
