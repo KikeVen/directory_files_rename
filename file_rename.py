4e4eeedb9b7c2010064f1f5a1b3023c07c562efe
KikeVen <bruzual.enrique@outlook.com>
2017-08-06 13:10:35 -0400

    formatting

--- file_rename.py
+++ file_rename.py
@@ -1,3 +1,5 @@
+# python file_rename.py
+
 import os
 import datetime
 
@@ -18,22 +20,22 @@ day_formated = day_formated.strftime("%m%d%Y")
 
 def file_rename():
     """
-    Lists all files in a specified directory (source_directory). Separates the file extention
-    from the file name (file_extention). If the file extentions are not includes in the exclusion 
-    list, then renames the files with the original file extention into another specified 
+    Lists all files in a specified directory (source_directory). Separates the file extension
+    from the file name (file_extension). If the file extensions are not includes in the exclusion 
+    list, then renames the files with the original file extension into another specified 
     directory (destination_directory) with a new name based on enumerator (my_enumerator), 
-    formated date (day_formated), time (time_formated) and original file extention (file_extention)
+    formated date (day_formated), time (time_formated) and original file extension (file_extension)
     """
 
     my_enumerator = 1
     for file in os.scandir(source_directory):
         if file.is_file():
-            file_extention = os.path.splitext(file.name)[1]
-            if file_extention != '.ini' and '.txt':
+            file_extension = os.path.splitext(file.name)[1]
+            if file_extension != '.ini' and '.txt':
                 print(str(my_enumerator) + time_formated +
-                      day_formated + file_extention)
+                      day_formated + file_extension)
                 os.rename(os.path.join(source_directory, file.name), os.path.join(
-                    destination_directory, str(my_enumerator) + time_formated + day_formated + file_extention))
+                    destination_directory, str(my_enumerator) + time_formated + day_formated + file_extension))
             my_enumerator += 1
 
 if __name__ == "__main__":
